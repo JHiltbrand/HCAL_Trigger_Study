2bc3a2240917d730189cb5593f94b2b8476ec7fd
Josh Hiltbrand <hiltbran@umn.edu>
2020-07-22 14:08:37 -0500

    Update algo_weights template file

--- scripts/algo_weights_template.py
+++ scripts/algo_weights_template.py
@@ -1,36 +1,35 @@
 import FWCore.ParameterSet.Config as cms
 
 pfaWeightsMap = {
-    # Remove before flight
     # Weights are in the form
-    # "WEIGHTS_VERSION" : cms.untracked.PSet(**dict([
-    #     ("1",   cms.untracked.vdouble(0.0)), 
-    #     ("2",   cms.untracked.vdouble(0.0)), 
-    #     ("3",   cms.untracked.vdouble(0.0)), 
-    #     ("4",   cms.untracked.vdouble(0.0)), 
-    #     ("5",   cms.untracked.vdouble(0.0)), 
-    #     ("6",   cms.untracked.vdouble(0.0)), 
-    #     ("7",   cms.untracked.vdouble(0.0)), 
-    #     ("8",   cms.untracked.vdouble(0.0)), 
-    #     ("9",   cms.untracked.vdouble(0.0)), 
-    #     ("10",  cms.untracked.vdouble(0.0)), 
-    #     ("11",  cms.untracked.vdouble(0.0)), 
-    #     ("12",  cms.untracked.vdouble(0.0)), 
-    #     ("13",  cms.untracked.vdouble(0.0)), 
-    #     ("14",  cms.untracked.vdouble(0.0)), 
-    #     ("15",  cms.untracked.vdouble(0.0)), 
-    #     ("16",  cms.untracked.vdouble(0.0)), 
-    #     ("17",  cms.untracked.vdouble(0.0)), 
-    #     ("18",  cms.untracked.vdouble(0.0)), 
-    #     ("19",  cms.untracked.vdouble(0.0)), 
-    #     ("20",  cms.untracked.vdouble(0.0)), 
-    #     ("21",  cms.untracked.vdouble(0.0)), 
-    #     ("22",  cms.untracked.vdouble(0.0)), 
-    #     ("23",  cms.untracked.vdouble(0.0)), 
-    #     ("24",  cms.untracked.vdouble(0.0)), 
-    #     ("25",  cms.untracked.vdouble(0.0)), 
-    #     ("26",  cms.untracked.vdouble(0.0)), 
-    #     ("27",  cms.untracked.vdouble(0.0)), 
-    #     ("28",  cms.untracked.vdouble(0.0))
-    # ]))
+    # "WEIGHTS_VERSION_NAME" : { 
+    #     "1"  : [0.0], 
+    #     "2"  : [0.0], 
+    #     "3"  : [0.0], 
+    #     "4"  : [0.0], 
+    #     "5"  : [0.0], 
+    #     "6"  : [0.0], 
+    #     "7"  : [0.0], 
+    #     "8"  : [0.0], 
+    #     "9"  : [0.0], 
+    #     "10" : [0.0], 
+    #     "11" : [0.0], 
+    #     "12" : [0.0], 
+    #     "13" : [0.0], 
+    #     "14" : [0.0], 
+    #     "15" : [0.0], 
+    #     "16" : [0.0], 
+    #     "17" : [0.0], 
+    #     "18" : [0.0], 
+    #     "19" : [0.0], 
+    #     "20" : [0.0], 
+    #     "21" : [0.0], 
+    #     "22" : [0.0], 
+    #     "23" : [0.0], 
+    #     "24" : [0.0], 
+    #     "25" : [0.0], 
+    #     "26" : [0.0], 
+    #     "27" : [0.0], 
+    #     "28" : [0.0]
+    # }
 }
